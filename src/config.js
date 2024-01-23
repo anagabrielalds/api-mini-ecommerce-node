d22e71ccfba3df014ba85e25f19debfdb504878b
anasantos-iteris <ana.santos@iteris.com.br>
2024-01-23 19:38:16 -0300

    Ajustando as variaveis de ambiente

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
+console.log(global.process.env.connectionString)
 module.exports = {
-    connectionString: '',
-    sendMailtrapUser: '',
-    sendMailtrapPassword: '',
-    containerConnectionString: ''
+    connectionString: global.process.env.connectionString,
+    sendMailtrapUser: global.process.env.sendMailtrapUser,
+    sendMailtrapPassword: global.process.env.sendMailtrapPassword,
+    containerConnectionString: global.process.env.containerConnectionString
 }
