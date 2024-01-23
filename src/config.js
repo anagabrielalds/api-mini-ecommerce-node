cd9e03c6303ffd31a37851afc5de84fd4be42802
anasantos-iteris <ana.santos@iteris.com.br>
2024-01-23 20:01:49 -0300

    adicionando o arquivo de configuração para o deploy no vercel

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
-console.log(global.process.env.connectionString)
 module.exports = {
     connectionString: global.process.env.connectionString,
     sendMailtrapUser: global.process.env.sendMailtrapUser,
